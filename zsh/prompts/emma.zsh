eb638364d9a6eb5a0c9ed9a03205db82afde39de
Emma Eilefsen Glenna <ee@it.no>
2023-09-15 12:59:26 +0200

    make repo nvim only

--- zsh/prompts/emma.zsh
+++
@@ -1,7 +0,0 @@
-prompt_emma_setup() {
-    PROMPT=' %F{yellow}%~%f %B%F{red}${vcs_info_msg_0_}%f%b$ '
-    RPS1='%B%F{blue}[%T]%f%b'
-    if [[ "$VIRTUAL_ENV" != "" ]]; then
-        RPS1='[%B%F{red}$(virtualenv_info)]%f%b $RPS1' 
-    fi
-}
--- zsh/zprofile
+++
@@ -1,3 +0,0 @@
-export LANG=en_US.UTF-8
-path+=$HOME/.cargo/env
-eval "$(/opt/homebrew/bin/brew shellenv)"
