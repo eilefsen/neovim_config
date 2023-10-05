34551648ef75ddbae6e72150ba044a77900ff2d6
Emma Eilefsen Glenna <ee@it.no>
2023-10-05 11:18:25 +0200

    update smartyank

--- after/plugin/smartyank.lua
+++ after/plugin/smartyank.lua
@@ -20,11 +20,7 @@ require('smartyank').setup {
     silent = false,         -- true to disable the "n chars copied" echo
     echo_hl = "Directory",  -- highlight group of the OSC52 echo message
   },
-  -- By default copy is only triggered by "intentional yanks" where the
-  -- user initiated a `y` motion (e.g. `yy`, `yiw`, etc). Set to `false`
-  -- if you wish to copy indiscriminately:
-  -- validate_yank = false,
-  -- 
+  validate_yank = true,     -- ensure yank is "intentional" (y, yy, Y, etc) before copying to system clipboard
   -- For advanced customization set to a lua function returning a boolean
   -- for example, the default condition is:
   -- validate_yank = function() return vim.v.operator == "y" end,
